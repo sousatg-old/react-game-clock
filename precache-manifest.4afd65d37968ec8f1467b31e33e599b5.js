2dd7ad753d46a1c7183e22a3bfa319d0038c8e62
Gil Sousa <tiagosousafl@gmail.com>
2019-08-17 14:24:41 +0100

    Updates

---
+++ precache-manifest.4afd65d37968ec8f1467b31e33e599b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95115f5bb33c4968fcaa05ef1643e66f",
+    "url": "/react-game-clock/index.html"
+  },
+  {
+    "revision": "6757942e024c8853fcdb",
+    "url": "/react-game-clock/static/css/main.0f2ef7e9.chunk.css"
+  },
+  {
+    "revision": "96e9b62d70833567fb07",
+    "url": "/react-game-clock/static/js/2.75dce891.chunk.js"
+  },
+  {
+    "revision": "6757942e024c8853fcdb",
+    "url": "/react-game-clock/static/js/main.ba7a2a6a.chunk.js"
+  },
+  {
+    "revision": "9e5fd2c576a3a35a1277",
+    "url": "/react-game-clock/static/js/runtime~main.31d49211.js"
+  },
+  {
+    "revision": "ff2b96719fb93f9698bfe4070dea4e27",
+    "url": "/react-game-clock/static/media/next.ff2b9671.svg"
+  },
+  {
+    "revision": "449c39789012b7ee860301a08ad2290d",
+    "url": "/react-game-clock/static/media/pause.449c3978.svg"
+  },
+  {
+    "revision": "066d8846ccbbc22a75bafbf6c89a6a9b",
+    "url": "/react-game-clock/static/media/reload.066d8846.svg"
+  }
+]);
