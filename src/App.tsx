e678dfb26840a5f81c4d9de6d46ed0080a4441e2
Gil Sousa <tiagosousafl@gmail.com>
2019-08-18 09:08:05 +0100

    Add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import {Route, HashRouter} from 'react-router-dom';
 import Clock from './pages/Clock/Clock';
 import Settings from './pages/Settings';
 import { SettingsProvider } from './context/SettingsContext';
+import ReactGA from 'react-ga';
+
+
+ReactGA.initialize('UA-83018916-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const basename = '/react-game-clock';
 
